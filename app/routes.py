3ea64ca1006d67641eba0ed365fb8a86d2cbb49b
kailin <herkailin@gmail.com>
2019-02-15 19:18:13 +0800

    use templates

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
 from app import abc
+from flask import render_template
+
 
 @abc.route('/')
 @abc.route('/abc')
 def home():
     return "Hello World!"
 
+
 @abc.route('/index')
 def index():
-    return "Index Page!"
+    user = { 'username': 'Kl'}
+    posts = [ {'author': 'John', 'body': 'A beautiful Day!'},
+              {'author': 'Susan', 'body': 'Avengers'}
+            ]
+
+
+    return render_template('index.html',  user=user, posts = posts )
